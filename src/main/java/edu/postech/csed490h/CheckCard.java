d0971baee6b98c36ec122c8ed73a4999756e34a5
shchae7 <shchae7@postech.ac.kr>
2024-02-27 05:47:39 +0900

    comment only frame added

---
+++ src/main/java/edu/postech/csed490h/CheckCard.java
@@ -0,0 +1,28 @@
+package test;
+
+public class CheckCard {
+    /* Check card keeps track of
+       bank name, account number, owner, and balance. */
+
+    /* An initialization function that creates a CreditCard instance */
+    public CheckCard(String owner, String bankName, int accountNumber, double initialBalance) {
+        ;
+    }
+
+    /* A function that makes payment (i.e. decreases balance)
+       and return true or false depending on if payment was successful */
+    public boolean makePaymentAndReturnSuccess(double amount) {
+        ;
+    }
+
+    /* A function that charges the card (i.e. increases balance)
+       and returns nothing (void) */
+    public void chargeCard(double amount) {
+        ;
+    }
+
+    /* A function that prints the overall status/information of given card */
+    public static void printStatus(CheckCard card) {
+        ;
+    }
+}
---
+++ src/main/java/edu/postech/csed490h/TestProgram.java
@@ -0,0 +1,18 @@
+package test;
+
+public class TestProgram {
+    public static void main(String[] args) {
+        // 1. Initialize check cards for three individuals (A, B, C) with initial balances
+
+        // 2. Print the overall status of all three credit cards
+
+        // 3. A successfully pays B but
+
+        /* A fails when attempting to pay C due to insufficient fund and
+           upon discovery of declined payment, charges card with additional funds. */
+
+        // 5. A, tries again, this time successfully
+
+        // 5. Print the updated status of all three credit cards
+    }
+}
